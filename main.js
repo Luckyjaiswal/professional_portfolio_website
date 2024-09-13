2fe7af75a345f4c80ff32a389797f0e4dd7f2ea7
Lucky Jaiswal <jaiswallucky264@gamil.com>
2024-09-13 11:50:59 +0530

    sds

---
+++ main.js
@@ -0,0 +1,33 @@
+var typed = new Typed(".text",{
+    strings: ["Frontend Developer","Web Developer","Nature Lover"],
+    typeSpeed:100,
+    backSpeed:100,
+    backDelay:1000,
+    loop:true
+});
+var typed = new Typed(".text1",{
+    strings: ["Frontend Developer","Web Developer","Nature Lover"],
+    typeSpeed:100,
+    backSpeed:100,
+    backDelay:1000,
+    loop:true
+});
+var typed = new Typed(".text2",{
+    strings: ["Frontend Developer","Web Developer","Nature Lover"],
+    typeSpeed:100,
+    backSpeed:100,
+    backDelay:1000,
+    loop:true
+});
+console.log("hello")
+
+// var navbar = document.querySelector('#NavBar');
+// var top = navbar.offsetTop;
+// function stickynavbar() {
+//   if (window.scrollY >= top) {    
+//     navbar.classList.add('sticky');
+//   } else {
+//     navbar.classList.remove('sticky');    
+//   }
+// }
+window.addEventListener('scroll', stickynavbar);
